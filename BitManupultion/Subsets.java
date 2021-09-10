5d39db4ee5d36663d64d43c0516b767a8d1b760c
dcr7978933k <b219021@iiit-bh.ac.in>
2021-09-09 22:14:53 -0700

    finding subsets using bitman

---
+++ BitManupultion/Subsets.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+
+        List<List<Integer>> ls= new ArrayList<List<Integer>>();
+        int n=nums.length;
+        
+        for(int i=0;i<(1<<n);i++){
+            
+            List<Integer> lsa= new ArrayList<Integer>();
+            
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0) 
+                    lsa.add(nums[j]);
+            }
+            ls.add(lsa);  
+        }
+        return ls;
+        
+    }
+}
