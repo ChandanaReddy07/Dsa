5ce92b41fad2747808bd415f7e2071a92529df2e
dcr7978933k <b219021@iiit-bh.ac.in>
2021-09-17 00:18:49 -0700

    making stack using queue

---
+++ Stack&queues/QueueToStack.java
@@ -0,0 +1,46 @@
+class MyStack {
+
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+   // private int top; 
+    
+
+    /** Initialize your data structure here. */
+    public MyStack() {
+        
+    }
+    
+    /** Push element x onto stack. */
+    public void push(int x) {
+         q1.add(x);
+         int sz = q1.size();
+         while (sz > 1) {
+             q1.add(q1.remove());
+             sz--;
+    }
+    }
+    
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        return  q1.remove();
+    }
+    
+    /** Get the top element. */
+    public int top() {
+        return q1.peek();
+    }
+    
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+         return q1.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
