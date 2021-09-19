869e78428d13d30c8dfcc8e9c2a85a8f7c156b52
dcr7978933k <b219021@iiit-bh.ac.in>
2021-09-19 06:40:48 -0700

    next greater elements

---
+++ Stack&queues/NextGrtElemt.java
@@ -0,0 +1,40 @@
+class Solution
+{
+    //Function to find the next greater element for each element of the array.
+    public static long[] nextLargerElement(long[] arr, int n)
+    { 
+        // Your code here
+        Stack<Long> s= new Stack<Long>();
+        
+        long[] v=new long[arr.length];
+        
+        //1 3 2 4
+        //3 4 4 -1
+     
+        for(int i=n-1;i>=0;i--){
+            
+            if(s.size()==0)
+            v[i]=-1;
+            else if(s.size()>0 && s.peek()>arr[i]) {
+                v[i]=s.peek();
+            }
+            else if(s.size()>0 && s.peek()<arr[i]){
+                while(s.size()>0 && s.peek()<=arr[i]){
+                    s.pop();
+                }
+                
+                if(s.size()==0){
+                    v[i]=-1;
+                }
+                else{
+                    v[i]=s.peek();
+                }
+            }
+            s.push(arr[i]);
+        }
+       // reverse(v,n);
+        return v;
+        
+    } 
+     
+}
