c2545af6255f764761084a108166f586af173b58
dcr7978933k <b219021@iiit-bh.ac.in>
2021-09-22 20:50:14 -0700

    java string token hr

---
+++ Strings/validAnagram.java
@@ -0,0 +1,21 @@
+static boolean isAnagram(String a, String b) {
+        
+    String str1= a.toLowerCase();
+    String str2= b.toLowerCase();
+    
+       if (str1.length() != str2.length()) {
+        return false;
+    }
+    int count[] = new int[256];
+    for (int i = 0; i < str1.length(); i++) {
+        count[str1.charAt(i)]++;
+        count[str2.charAt(i)]--;
+    }
+    for (int i = 0; i < 256; i++) {
+        if (count[i] != 0) {
+            return false;
+        }
+    }
+    return true;
+
+}
