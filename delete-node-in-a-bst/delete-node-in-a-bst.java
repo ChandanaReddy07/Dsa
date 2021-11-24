55d74a0f83102279f8a28d73ef2cfe73473ad7ae
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2021-11-24 20:53:56 +0530

    Time: 0 ms (100.00%), Space: 46.7 MB (33.61%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.java
+++ delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -21,21 +21,17 @@ class Solution {
         
      public TreeNode deleteRec(TreeNode root, int key)
     {
-        /* Base Case: If the tree is empty */
+       
         if (root == null)
             return root;
  
-        /* Otherwise, recur down the tree */
         if (key < root.val)
             root.left = deleteRec(root.left, key);
         else if (key > root.val)
             root.right = deleteRec(root.right, key);
  
-        // if key is same as root's
-        // key, then This is the
-        // node to be deleted
         else {
-            // node with only one child or no child
+          
             if (root.left == null)
                 return root.right;
             else if (root.right == null)
@@ -44,7 +40,6 @@ class Solution {
           
             root.val = minValue(root.right);
  
-            // Delete the inorder successor
             root.right = deleteRec(root.right, root.val);
         }
  
