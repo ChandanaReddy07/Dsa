0b90c44295f1ad78af5560ac8f0ea6a67aeb25f2
dcr7978933k <b219021@iiit-bh.ac.in>
2021-12-10 12:22:43 +0530

    perfect sum using 01knapsack

---
+++ DP/perfectSum.java
@@ -0,0 +1,27 @@
+ class perfectSum {
+
+	public int perfectSum(int nums[],int n, int sum) 
+	{ 
+	    // Your code goes here
+	     int t[][] = new int[n+1][sum+1];
+        for(int i=0;i<sum+1;i++){
+            t[0][i]=0; 
+        }
+        for(int i=0;i<n+1;i++){
+            t[i][0]=1; 
+        }
+          int mod = 1000000007;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<sum+1;j++){
+                if(nums[i-1]<=j){
+                    t[i][j] =(t[i-1][j] + t[i-1][j-nums[i-1]])% mod;
+                }else{
+                    t[i][j] = t[i-1][j]% mod;
+                }
+            }
+        }
+        return t[n][sum];
+	
+	} 
+}
