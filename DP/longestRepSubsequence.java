081dff0b166c6229e5e04121171705b70a98522f
dcr7978933k <b219021@iiit-bh.ac.in>
2021-12-21 13:12:48 +0530

    lps

---
+++ DP/longestRepSubsequence.java
@@ -0,0 +1,29 @@
+class Solution
+{
+    public int LongestRepeatingSubsequence(String str)
+    {
+        // code here
+    int x=str.length();
+    int y=x;
+        // your code here
+         int[][] t= new int[x+1][y+1];
+          for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                if(i==0 || j==0)
+                  t[i][j]=0;
+        }
+        }
+        
+          for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                if(str.charAt(i-1)==str.charAt(j-1) && i!=j)
+                  t[i][j]=1+ t[i-1][j-1];
+               else
+                 t[i][j] = Math.max( t[i-1][j] , t[i][j-1]);
+        
+        }
+        }
+       
+        return t[x][y];
+    }
+}
