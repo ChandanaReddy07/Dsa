80e27c8fabbc2d62795a1b9877589508ae9040ee
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-01-01 23:37:17 +0530

    lps approach

---
+++ DP/MinimumNoofDeletiontomakePalindrome.java
@@ -0,0 +1,30 @@
+  static int minimumNumberOfDeletions(String s1) {
+        //your code here
+         StringBuilder s2 = new StringBuilder();
+ 
+        s2.append(s1);
+ 
+    
+        s2.reverse();
+         int x=s1.length(),y=s2.length();
+	    // Your code goes here
+	     int[][] t= new int[x+1][y+1];
+          for(int i=0;i<=x;i++){
+            for(int j=0;j<=y;j++){
+                if(i==0 || j==0)
+                  t[i][j]=0;
+        }
+        }
+        
+          for(int i=1;i<=x;i++){
+            for(int j=1;j<=y;j++){
+                if(s1.charAt(i-1)==s2.charAt(j-1))
+                  t[i][j]=1+ t[i-1][j-1];
+               else
+                 t[i][j] = Math.max( t[i-1][j] , t[i][j-1]);//kyuki it is subsequence thats y gapes r allowed
+        
+        }
+        }
+       
+        return x-t[x][y];
+    }
