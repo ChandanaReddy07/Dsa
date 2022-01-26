65e6696b7efb63551ae26d93e738056e9486ac2d
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-01-26 23:14:37 +0530

    Time: 5 ms (88.59%), Space: 39.2 MB (70.85%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+         HashMap<Integer,Integer> hm = new HashMap<>();
+    
+	int c = 0; //for counting distinct values 
+    for(int i=0;i<nums.length;i++){
+        if(hm.containsKey(nums[i])){
+            int v = hm.get(nums[i]);
+            v++;
+            hm.put(nums[i],v);
+        }else{
+            hm.put(nums[i],1);
+            c++;
+        }
+    }
+         PriorityQueue<int[]> p = new PriorityQueue<>((a,b) -> {
+            if(a[0] == b[0]) {
+                return   b[1]-a[1];
+            } else {
+                return  a[0] - b[0];
+            }
+        });
+        
+        for (Integer key : hm.keySet()) {          
+        p.add(new int[]{hm.get(key), key}); // log k         
+    }
+        int j=0;
+         for (int i=0;i<c;i++) {
+             
+             int val=p.poll()[1];
+             int freq= hm.get(val);
+             
+              for (int k=0;k<freq;k++) {
+                  
+                  nums[j]=val;
+                  j++;
+       
+         }
+           
+         }
+            
+           
+    return nums;
+    
+    }
+}
