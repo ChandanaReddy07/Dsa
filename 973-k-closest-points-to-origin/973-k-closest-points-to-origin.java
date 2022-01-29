6a1f7dd5829227818eca30a616c0f45ddb1a1239
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-01-29 13:38:59 +0530

    Time: 30 ms (50.88%), Space: 51 MB (41.14%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -3,42 +3,34 @@ class Solution {
         
         int[][] res=new int[k][2];
         
-        //create a map
-        HashMap<Integer,Integer> hm= new HashMap<>();
         int n=points.length;
         
-        for(int i=0;i<n;i++){
-            
-            int dist = (points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
-            
-            hm.put(i,dist);
-            
-        }
-        
+        //min heap
         PriorityQueue<int[]> p = new PriorityQueue<>((a,b) -> {
             if(a[0] == b[0]) {
                 return   b[1]-a[1];
             } else {
-                return a[0]-  b[0];
+                return b[0]-  a[0];
             }
         });
         
-          for (Integer key : hm.keySet()) {          
-        p.add(new int[]{hm.get(key), key});       
-    }
+        for (int i = 0; i < n; i++) {
+            int dist=(points[i][0]*points[i][0]) + (points[i][1]*points[i][1]);
+            int[] entry = {dist, i};
+            if (p.size() < k) {
+                
+                p.add(entry);
+            } else if (entry[0] < p.peek()[0]) {
+                p.poll();
+                p.add(entry);
+            }
+        }
         
-         for (int i=0;i<k;i++) {
-             
-             // System.out.println(p.poll()[1]);
-             int y=p.poll()[1];
-                 // System.out.println(y);
-             res[i][0]=points[y][0];
-              res[i][1]=points[y][1];
-   
-         }
-           // res[0][0]=-2;
-           //    res[0][1]=2;
-        
+         for (int i = 0; i < k; i++) {
+            int entryIndex = p.poll()[1];
+            res[i] = points[entryIndex];
+        }
+       
         return res;
    
     }
