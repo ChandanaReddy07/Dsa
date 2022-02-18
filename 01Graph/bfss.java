94a1b7cf3c84c2000972e28374c9c334b281a15a
ChandanaReddy07 <b219021@iiit-bh.ac.in>
2022-02-18 19:39:26 +0530

    updated

---
+++ 01Graph/bfss.java
@@ -0,0 +1,32 @@
+
+public static void Bfs(int s){
+
+    boolean visited[] = new boolean(V);
+
+    //make queue
+    LinkedList<Integer> queue= new LinkedList<>();
+
+    queue.add(s);
+    visited[0]=true;
+
+    while(queue.size()!=0){
+
+        int p=queue.pop();//dequeue
+
+        System.out.println(p);
+
+        Iterator<Integer> it = graph[p].listIterator();
+
+        while(it.hasNext){
+            int n =it.next();
+            if(!visited[n]){
+                queue.add(n);
+                visited[n]=true;
+
+            }
+        }
+  
+    }
+
+
+}
