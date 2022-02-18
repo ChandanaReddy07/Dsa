f01cfdc4788450304b8f41c8e878f886ab7f25c9
ChandanaReddy07 <b219021@iiit-bh.ac.in>
2022-02-18 19:23:33 +0530

    started graph

---
+++ 01Graph/constructGraph.java
@@ -0,0 +1,21 @@
+class Graph
+{
+    private int V;   // No. of vertices
+    private LinkedList<Integer> adj[]; //Adjacency Lists
+ 
+    // Constructor
+    Graph(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+ 
+    // Function to add an edge into the graph
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+    }
+ 
+}
