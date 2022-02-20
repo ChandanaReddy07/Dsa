6e85f1265d3408d66b78e0da66edd6cd8f5a4e38
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-02-20 14:11:43 +0530

    Added solution - LeetHub

--- Smallest Absolute Difference - GFG/smallest-absolute-difference.java
+++ Smallest Absolute Difference - GFG/smallest-absolute-difference.java
@@ -44,7 +44,7 @@ class Compute {
         int diff= (int) (arr[(int)n-1]-arr[0]);
         int l=0;
         int h=diff;
-        while(l<h){
+        while(l<h){// l and h are min and max diff possible
             int m=(l+h)/2;
             if(ok(m,arr,(int)n,(int)k))
                 h=m;
@@ -56,11 +56,13 @@ class Compute {
     public static boolean ok(int m,long[] arr,int n,int k){
         int j=1;
         int total=0;
+        
+     
         for(int i=0;i<n;i++){
             while(j<n && arr[j]-arr[i]<=m) j++;
             j--;
             int x=(j-i);
-            total+=x;
+            total+=x;   //kitne saree possiple diff jo m se less hai
         }
         return (total>=k);
     }
