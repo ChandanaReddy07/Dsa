53a0814724d3d2dd9fc75b8e4fae6d39bf7e373d
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-07-24 00:30:20 +0530

    Time: 2 ms (80.95%), Space: 49 MB (71.55%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int jump(int[] arr) {
+        int n=arr.length;
+        int i=0;
+        int count=0;
+        
+        if(n==1 ){
+                  return 0;
+                 }
+        
+         while(i<n){
+            
+            int max=i+arr[i];
+            int jump=i;
+            // System.out.print("max="+i);
+             
+              if(max>=n-1 ){
+                  return ++count;
+                 }
+            for(int j=i+1;j<=i+arr[i];j++){
+
+                if(max<arr[j]+j){
+                    max=arr[j]+j;
+                    jump=j;
+                    // System.out.print("ha"+jump+"max="+max);
+                }
+                   if(jump==n-1){                            
+                    System.out.print("ha7");
+                     return ++count;
+                 }
+           
+            }
+            if(jump>i){
+                count++;
+                i=jump;
+            }     
+        }
+        return 2;
+    }
+}
