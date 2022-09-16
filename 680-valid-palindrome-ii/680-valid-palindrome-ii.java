f2c0c5bd0f2ad5a3b9c81957b97a5a603acc8a7d
D Chandana Reddy <57955783+ChandanaReddy07@users.noreply.github.com>
2022-09-17 00:16:40 +0530

    Time: 13 ms (48.79%), Space: 54.6 MB (47.73%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -1,26 +1,19 @@
 class Solution {
     public boolean validPalindrome(String s) {
         
-         int t1=0, t2=-1;
         int n =s.length();
-        for (int i=0 ; i<n/2 ; i++)
+        int i=0,j=n-1;
+        while(i<j)
         {
-            if (s.charAt(i)!=s.charAt(n-1-i))
+            if (s.charAt(i)!=s.charAt(j))
             {
-                t1 = i;
-                t2 = n-1-i;
-                break;
+             return is_pal(s.substring(i+1,j+1)) || 
+            is_pal(s.substring(i,j));
             }
+            i++;
+            j--;
         }
-        
-        if (t2 == -1)
-            return true; 
-        
-     return is_pal(s.substring(0, t1) + s.substring(t1+1,n)) || 
-            is_pal(s.substring(0, t2) + s.substring(t2+1,n));   
-    
-        
-        
+            return true;    
     }
     
     boolean is_pal(String s) 
